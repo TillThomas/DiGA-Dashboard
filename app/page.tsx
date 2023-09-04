b0fcf33f3ac994cc8c4adb91cc5080bc415840fa
Till <Till.Thomas.tt@gmail.com>
2023-09-04 15:25:22 +0200

    init area

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+'use client'
+
+
+import Area from "@/components/area";
+import ParentSize from '@visx/responsive/lib/components/ParentSize';
 
 export default function Page() {
-  return <h1>DiGA</h1>
+  return <><h1>DiGA</h1><div><ParentSize>{({ width, height }) => <Area width={width} height={1000}></Area>}</ParentSize></div></>
 }
